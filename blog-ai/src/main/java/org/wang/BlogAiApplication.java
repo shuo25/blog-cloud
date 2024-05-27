acad202abb71aadd10a575bc7b5e7648ad2180df
王硕 <18615498639@163.com>
2024-05-27 18:53:50 +0800

    deps: 引入SpringCloudAlibabaAI 相关包

---
+++ blog-ai/src/main/java/org/wang/BlogAiApplication.java
@@ -0,0 +1,11 @@
+package org.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAiApplication.class);
+    }
+}
